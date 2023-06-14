514e35a5968aa7ec17f3c27b562dbaa33b3a5032
kora1348 <amelianekor@gmail.com>
2023-06-14 23:13:10 +0200

    totoh

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from './todo.service';
 import { Tit, TotoResponse } from './types/toto.response';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,9 +12,37 @@ import { Tit, TotoResponse } from './types/toto.response';
 export class TodoComponent implements OnInit {
   filteredTIT: Tit[] = [];
   
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService, private router: Router) { }
 
   ngOnInit() {
     this.filteredTIT = this.todoService.TIT;
   }
+
+
+   
+    onActionChange(event: Event, svm: number) {
+    const target = event.target as HTMLSelectElement;
+    const action = target.value;
+    
+    if (action === 'edit') {
+    this.navigateToEdit(svm);
+    } else if (action === 'delete') {
+    this.navigateToDelete(svm);
+    }
+    }
+    
+    navigateToEdit(svm: number) {
+    this.router.navigate(['/functional-management/edit-purchasable-securities', svm]);
+    }
+    
+    navigateToDelete(svm: number) {
+    this.router.navigate(['/functional-management/delete-purchasable-securities', svm]);
+    }
+    
+    addPurchasableSecurities() {
+    this.router.navigate(['functional-management/add-purchasable-securities']);
+    }
+    
+
+
 }
